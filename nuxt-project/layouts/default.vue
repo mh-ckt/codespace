8f0ee469c3f3a1091e32b6c2034b7ded3d8e5be4
陈可涛 <cacao.chen@heavengifts.com>
2025-07-31 10:17:07 +0800

    nuxt project init

---
+++ nuxt-project/layouts/default.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+// import '~/public/static/css/swiper.min.css'
+import '~/assets/scss/common.css'
+
+useHead({
+  script: [
+    // { type: 'text/javascript', src: '/static/js/jquery-3.7.1.min.js' },
+    // { type: 'text/javascript', src: '/static/js/swiper.js' },
+  ],
+})
+</script>
+
+<template>
+  <!-- <PublicHeader /> -->
+  <slot />
+  <!-- <PublicFooter /> -->
+</template>
---
+++ nuxt-project/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CaseList from './case-list.vue';
+
+</script>
+
+<template>
+  <CaseList />
+</template>
+
+<style></style>
