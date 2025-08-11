d3a7a731415d09f8e8b513208a5a3623b71a24c0
陈可涛 <cacao.chen@heavengifts.com>
2025-08-11 14:38:48 +0800

    feat: 利用masonry-layout插件是实现一个简单的瀑布流布局

--- vue-project/src/views/FuncView.vue
+++ vue-project/src/views/FuncView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 // import LazyImages from '@/components/LazyImages/index.vue'
-import Calendar from '@/components/Calendar/index.vue'
+// import Calendar from '@/components/Calendar/index.vue'
+import FallsLayout from "@/components/FallsLayout/index.vue"
 </script>
 
 <template>
   <!-- <LazyImages /> -->
-   <Calendar/>
+  <!-- <Calendar/> -->
+  <FallsLayout />
 </template>
